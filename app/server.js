be07b4e87a8c13b218ada4a5e9af0dcf4097eabc
Benjamin Workman <bdworkma@gmail.com>
2026-08-26 21:41:22 -0400

    First commit Two Tests

--- app/server.js
+++ app/server.js
@@ -1,31 +1,74 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml'); // Replace './swagger.yaml' with the path to your Swagger file
+const express = require("express");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml"); // Replace './swagger.yaml' with the path to your Swagger file
 const app = express();
 
 app.use(bodyParser.json());
 
 // Importing the data from JSON files
-const users = require('../initial-data/users.json');
-const brands = require('../initial-data/brands.json');
-const products = require('../initial-data/products.json');
+const users = require("../initial-data/users.json");
+const brands = require("../initial-data/brands.json");
+const products = require("../initial-data/products.json");
+
+authenticate = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, "yoursecretkey");
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "User Authentication Failed" });
+  }
+};
+
+// Get /api/brands
+
+app.get("/api/brands", (req, res) => {
+  res.status(200).json(brands);
+});
+
+//Login
+app.post("/api/login", (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find((user) => user.login.username == username);
+
+  if (!user) {
+    return res.status(401).json({
+      message: "User not found",
+    });
+  }
+
+  if (user.login.password !== password) {
+    return res.status(401).json({
+      message: "Incorrect Password",
+    });
+  }
+
+  const token = jwt.sign({ userId: user.id }, "yoursecretkey");
+
+  res.status(200).json({
+    message: "Auth successful",
+    token: token,
+  });
+});
 
 // Error handling
 app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).send('Something broke!');
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 // Swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Starting the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
--- test/server.test.js
+++ test/server.test.js
@@ -1,14 +1,64 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const server = require('../app/server'); // Adjust the path as needed
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../app/server"); // Adjust the path as needed
+const brands = require("../initial-data/brands.json");
 
 const should = chai.should();
 chai.use(chaiHttp);
 
 // TODO: Write tests for the server
 
-describe('Brands', () => {});
+//Test for GET /api/brands
+describe("Brands", () => {
+  it("it should return all brands from brands.json ", (done) => {
+    chai
+      .request(server)
+      .get("/api/brands")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.an("array");
+        res.body.should.deep.equal(brands);
+        done();
+      });
+  });
+});
 
-describe('Login', () => {});
+//Test for POST /api/login
+describe("Login", () => {
+  it("it should test if a user successfully logs in", (done) => {
+    chai
+      .request(server)
+      .post("/api/login")
+      .send({
+        username: "greenlion235",
+        password: "waters",
+      })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property("token");
+        res.body.should.have.property("message").eql("Auth successful");
 
-describe('Cart', () => {});
+        done();
+      });
+  });
+
+  it("it should fail login with incorrect password", (done) => {
+    chai
+      .request(server)
+      .post("/api/login")
+      .send({
+        username: "testuser",
+        password: "wrongpassword",
+      })
+      .end((err, res) => {
+        res.should.have.status(401);
+        res.body.should.have
+          .property("message")
+          .eql("User not found" || "Incorrect Password");
+        done();
+      });
+  });
+});
+
+// Test for GET /api/me.cart
+describe("Cart", () => {});
